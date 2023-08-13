bd851f7d9d3ce03f7158ff53c25ad06e72fcf5d1
carlostorresfiuni19999 <carlos.torres@fiuni.edu.py>
2023-08-13 19:36:53 -0400

    Borrar Productos

--- public/js/controller/productoController.js
+++ public/js/controller/productoController.js
@@ -70,7 +70,7 @@ const guardarProducto = async () => {
             errorImgFormat.className = "oculto";
             errorImgLen.className = "oculto";
             errorImgReq.className = "oculto";
-            
+
             const producto = new ProductoCreate(
                 nombre, descripcion, price, selectedCat, imagenInput
             )
@@ -102,14 +102,14 @@ const guardarProducto = async () => {
 const getProductosTable = async () => {
     showSpinner(spinnerProductos);
     const buff = [];
-    const productos =  await getsHttp(`${URL_SERVER}/productos`, "");
+    const productos = await getsHttp(`${URL_SERVER}/productos`, "");
     console.log(productos);
     offSpinner(spinnerProductos);
-    if(productos.status != 200) {
+    if (productos.status != 200) {
         alert("Ocurrio un error, verifique su conexion de internet.");
-        
+
     } else {
-        if(productos.data && productos.data.length > 0) {
+        if (productos.data && productos.data.length > 0) {
             buff.push('<table class="table table-striped">');
             buff.push('<thead>')
             buff.push('<tr>')
@@ -121,9 +121,9 @@ const getProductosTable = async () => {
             buff.push('</thead>')
             buff.push('<tbody>');
             let i = 0;
-            for(let val of productos.data){
+            for (let val of productos.data) {
                 buff.push(
-                `<tr>
+                    `<tr>
                      <th scope = "row"> ${i} </td>
                      <td> ${val.nombre} </td>
                      <td> ${val.descripcion} </td>
@@ -139,7 +139,7 @@ const getProductosTable = async () => {
                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                             </svg>
                         </button>
-                        <button class="btn btn-outline-danger" onclick="borrar('${val._id}')"> 
+                        <button class="btn btn-outline-danger" onclick="borrarProducto('${val._id}')"> 
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
                             <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
@@ -147,16 +147,34 @@ const getProductosTable = async () => {
                         </button>
                     </td>
                 </tr>`
-                     
+
                 );
-                i ++;           }
+                i++;
+            }
             buff.push('</tbody>')
             buff.push('</table>');
-            
+
             let tabla = document.querySelector("#table-productos");
             tabla.innerHTML = buff.join("\n");
         }
     }
 
+}
+
+const borrarProducto = async (id) => {
+    const verify = confirm("Estas seguro que deseas borrar?, esta accion es irreversible");
+
+    if (verify) {
+        showSpinner(spinnerProductos);
+        const peticion = await deleteHttp(`${URL_SERVER}/productos`, '', id);
+        offSpinner(spinnerProductos);
+
+        if(peticion){
+            await getProductosTable();
+        } else {
+            alert("Ocurrio un error al borrar el producto, revisa su conexion");
+        }
+
+    }
 }
 
